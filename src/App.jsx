c0162c9f29cc10cb66d128a80efc5f61fe18d674
Denison Kolling <denisonkolling@gmail.com>
2023-08-04 09:12:02 -0300

    add profile and input

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
-import Header from './components/Header'
-import './App.css'
+import Header from './components/Header';
+import background from '././assets/background.svg';
+import background2 from '././assets/github.svg';
+import './App.css';
 
 function App() {
-
-
-  return (
-    <>
-    <Header />
-    </>
-  )
+	return (
+		<>
+			<Header />
+			<div className="content">
+				<img src={background2} className="background" alt="background app" />
+				<div className="info">
+          <div>
+					<div>
+						<input name="user" placeholder="@username" />
+						<button>Search</button>
+					</div>
+				</div>
+				<div className="profile">
+					<img
+						src="https://avatars.githubusercontent.com/u/115804868?v=4"
+						className="profile-picture"
+					/>
+					<div>
+						<h3>Denison Kolling</h3>
+						<p>Descrição</p>
+					</div>
+				</div>
+        </div>
+			</div>
+		</>
+	);
 }
 
-export default App
+export default App;

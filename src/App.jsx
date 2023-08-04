a4392b60f54114dc00f0f7972c333b94d84fcd6f
Denison Kolling <denisonkolling@gmail.com>
2023-08-04 11:04:48 -0300

    add repositories items list

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,40 @@
 import Header from './components/Header';
-import background from '././assets/background.svg';
-import background2 from '././assets/github.svg';
+import background from '././assets/github.svg';
 import './App.css';
+import ItemList from './components/ItemList';
 
 function App() {
 	return (
 		<>
 			<Header />
 			<div className="content">
-				<img src={background2} className="background" alt="background app" />
+				<img src={background} className="background" alt="background app" />
 				<div className="info">
-          <div>
 					<div>
-						<input name="user" placeholder="@username" />
-						<button>Search</button>
+						<div>
+							<input name="user" placeholder="@username" />
+							<button>Search</button>
+						</div>
+					</div>
+					<div className="profile">
+						<img
+							className="profile-picture"
+							src="https://avatars.githubusercontent.com/u/115804868?v=4"
+						/>
+						<div>
+							<h3>Denison Kolling</h3>
+							<span>@denisonkolling</span>
+							<p>description</p>
+						</div>
+					</div>
+					<hr />
+					<div>
+						<h4>Repositories</h4>
+						<ItemList title='map-react' description='a react map application' />
+						<ItemList title='map-react' description='a react map application' />
+						<ItemList title='map-react' description='a react map application' />
 					</div>
 				</div>
-				<div className="profile">
-					<img
-						src="https://avatars.githubusercontent.com/u/115804868?v=4"
-						className="profile-picture"
-					/>
-					<div>
-						<h3>Denison Kolling</h3>
-						<p>Descrição</p>
-					</div>
-				</div>
-        </div>
 			</div>
 		</>
 	);
